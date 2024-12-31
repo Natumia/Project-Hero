10f8cf54be16f49abee181db5dadfcd79564b2e2
Natumia <greatnatumia@gmail.com>
2024-12-31 13:40:43 -0500

    With a small bit of inspiration I fixed my rebound and it is just a renamed duplicate of Flinch. It works well though.
    Update player.tscn, zol.gd, and zol.tscn

--- Entities/Zol/zol.gd
+++ Entities/Zol/zol.gd
@@ -16,6 +16,9 @@ func _process(_delta: float) -> void:
 func _on_hurt_box_area_entered(_area: Area2D) -> void:
 	$StateMachine.change_state("flinch")
 
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	$StateMachine.change_state("rebound")
+
 func _on_health_target_hit(value) -> void:
 	$HealthBar.value = value
 
